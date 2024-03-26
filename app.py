ee6101e239ebf85a64074a667658df3bddc280a8
yaqub <yaqubawmusse123@gmail.com>
2024-03-26 22:31:06 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+character_name = "Yaqub"
+character_age = "40"
+is_Male = False
+print("there once was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+character_name = "Mike"
+print("He really liked the name " + character_name + ",")
+print("but didn't like being " + character_age + ".")
